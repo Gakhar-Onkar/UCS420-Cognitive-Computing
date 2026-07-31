384dc820bd50266444c673ee8294df9733d3b693
Onkar Gakhar <gakharonkar1@gmail.com>
2026-07-31 09:19:37 +0530

    Add basic arithmetic and loop operations in assignments.py
    The script performs various arithmetic operations, prints sequences, and takes user input for calculations.

---
+++ assignments.py
@@ -0,0 +1,40 @@
+a=3
+b=4
+g=6
+c="Hello "
+d="World, "
+e=c+d
+f=a+b
+print(f)
+print(e)
+print("Onkar\n"*3)
+print(a+b+g)
+h="I am Onkar"
+print(c+d+h)
+l=input("Enter age :")
+print(l)
+print("\n")
+for i in range(1, 10, 3):
+    print(i)
+i=1
+while i<5:
+    print(i)
+    i=i+1
+for i in range(1, 11, 1):
+    print(i*5)
+for i in range(1, 11, 1):
+    print("Hello World")
+print(list(range(10,20,2)))
+print(list(range(-10,-20,2)))
+for i in range(1, 11, 1):
+    print(i*7)
+for i in range(1, 11, 1):
+    print(i*9)
+k=int(input("Enter no :"))
+for i in range(1, 11, 1):
+    print(i*k)
+m=int(input("Enter no :"))
+n=0
+for i in range(1, m, 1):
+    n=n+i
+print(n)
